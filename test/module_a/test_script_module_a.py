1275b47bf4127f330c21644fa850da9cae069a70
Maati Raissi <maati.raissi@capgemini.com>
2024-05-24 15:48:44 +0200

    tests

---
+++ test/module_a/test_script_module_a.py
@@ -0,0 +1,9 @@
+import re
+import time
+
+assert 1 == 1
+if (
+    "hello world i'm here and the string is too long and another really long string that'll go above 88"
+    == "2"
+):
+    print("ok")
